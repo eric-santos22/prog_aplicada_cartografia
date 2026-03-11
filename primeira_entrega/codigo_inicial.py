8ad916a4c73f2721e6664ea83a857b2a05cebb1a
eric-santos22 <ericduarteds@gmail.com>
2026-03-11 09:45:44 -0300

    Downloader class

--- primeira_entrega/codigo_inicial.py
+++ primeira_entrega/codigo_inicial.py
@@ -1,1 +1,13 @@
-print("Testando o primeiro código")
+import pandas as pd
+import geopandas as gpd
+
+
+df = pd.read_csv("pontos.csv")
+
+gdf = gpd.GeoDataFrame(df,
+                       geometry=gpd.points_from_xy(
+                           df.lon, df.lat),
+                           crs="EPSG:4326"
+                       )
+
+print(gdf.head())
